cdf1d0c406daeab9ddb7ac54e7ad2ed982b2e363
JP <jplmarotinho22@gmail.com>
2025-05-06 10:42:46 -0300

    IncorrectEmailOrPasswordException explicit Exception

---
+++ src/main/java/com/tuorjp/financial_helper/exception/IncorrectEmailOrPasswordException.java
@@ -0,0 +1,9 @@
+package com.tuorjp.financial_helper.exception;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class IncorrectEmailOrPasswordException extends AuthenticationException {
+    public IncorrectEmailOrPasswordException(String message) {
+        super(message);
+    }
+}
