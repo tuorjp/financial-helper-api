39aec4d76a26889e2522b735da30264a990de5c5
JP <jplmarotinho22@gmail.com>
2025-05-06 10:41:58 -0300

    AuthController exception handler moved to ControllerExceptionHandler

--- src/main/java/com/tuorjp/financial_helper/controllers/AuthController.java
+++ src/main/java/com/tuorjp/financial_helper/controllers/AuthController.java
@@ -1,8 +1,9 @@
 package com.tuorjp.financial_helper.controllers;
 
 import com.tuorjp.financial_helper.dto.UserDTO;
+import com.tuorjp.financial_helper.exception.IncorrectEmailOrPasswordException;
 import com.tuorjp.financial_helper.security.JwtUtil;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,24 +15,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class AuthController {
-    @Autowired
-    private AuthenticationManager authenticationManager;
-
-    @Autowired
-    private JwtUtil jwtUtil;
-
-    @Autowired
-    private UserDetailsService userDetailsService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
 
     @PostMapping("/v1/authenticate")
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody UserDTO dto) throws Exception{
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody UserDTO dto){
         try {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword())
             );
         } catch (AuthenticationException e) {
-            throw new Exception("Incorrect email or password", e);
+            throw new IncorrectEmailOrPasswordException("Incorrect email or password.");
         }
 
         final UserDetails userDetails = userDetailsService.loadUserByUsername(dto.getEmail());
